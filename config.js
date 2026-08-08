c3bd2515d8820453408bf860df0c9ae96b7cc79a
carolavargasmunasur <cvargas@munasur.org>
2026-08-08 15:24:22 -0300

    Primera versión MUNASUR Misiones

---
+++ config.js
@@ -0,0 +1,4 @@
+// Pegá acá la URL /exec de tu implementación de Google Apps Script.
+window.MUNASUR_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbw3D7HJ2_9tKaTDufjmJy4lPukTg0EtzwtpdvM-arx6gufwWqiKQI4u2TNVMELOC9ny/exec"
+};
